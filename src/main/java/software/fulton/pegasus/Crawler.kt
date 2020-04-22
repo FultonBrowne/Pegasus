28a81ac13ce76f445d8546400af87be3d04a2f0f
fulton <gprlabs@outlook.com>
2020-04-22 15:50:30 -0700

    working on awesomeness

---
+++ src/main/java/software/fulton/pegasus/Crawler.kt
@@ -0,0 +1,10 @@
+package software.fulton.pegasus
+
+import com.github.xbynet.crawler.Processor
+import com.github.xbynet.crawler.Response
+
+class Crawler: Processor() {
+    override fun process(resp: Response?) {
+        TODO("Not yet implemented")
+    }
+}
