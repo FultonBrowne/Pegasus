cb00d4fdc6db769f79e4d82a501f572052c9d42f
fulton <gprlabs@outlook.com>
2020-04-23 11:05:37 -0700

    some crawler stuff working

--- src/main/java/software/fulton/pegasus/Spider.java
+++ src/main/java/software/fulton/pegasus/Spider.java
@@ -1,9 +1,5 @@
 package software.fulton.pegasus;
 
-import edu.uci.ics.crawler4j.crawler.Page;
-import edu.uci.ics.crawler4j.crawler.WebCrawler;
-import edu.uci.ics.crawler4j.parser.HtmlParseData;
-import edu.uci.ics.crawler4j.url.WebURL;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,85 +7,73 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 public class Spider {
 
-        // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
-        private static final String USER_AGENT =
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
-        private List<String> links = new LinkedList<String>();
-        private Document htmlDocument;
+    // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
+    private static final String USER_AGENT =
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    private Document htmlDocument;
+    private ArrayList<String> beenTo = new ArrayList<String>();
 
 
-        /**
-         * This performs all the work. It makes an HTTP request, checks the response, and then gathers
-         * up all the links on the page. Perform a searchForWord after the successful crawl
-         *
-         * @param url
-         *            - The URL to visit
-         * @return whether or not the crawl was successful
-         */
-        public boolean crawl(String url)
-        {
-            try
+    /**
+     * This performs all the work. It makes an HTTP request, checks the response, and then gathers
+     * up all the links on the page. Perform a searchForWord after the successful crawl
+     *
+     * @param url - The URL to visit
+     * @return whether or not the crawl was successful
+     */
+    public boolean crawl(String url) {
+        if (beenTo.contains(url)){
+            return false;
+        }
+        beenTo.add(url);
+        try{
+
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            this.htmlDocument = htmlDocument;
+            if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+            // indicating that everything is great.
             {
-                Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
-                Document htmlDocument = connection.get();
-                this.htmlDocument = htmlDocument;
-                if(connection.response().statusCode() == 200) // 200 is the HTTP OK status code
-                // indicating that everything is great.
-                {
-                    System.out.println("\n**Visiting** Received web page at " + url);
-                }
-                if(!connection.response().contentType().contains("text/html"))
-                {
-                    System.out.println("**Failure** Retrieved something other than HTML");
-                    return false;
-                }
-                Elements linksOnPage = htmlDocument.select("a[href]");
-                System.out.println("Found (" + linksOnPage.size() + ") links");
-                for(Element link : linksOnPage)
-                {
-                    this.links.add(link.absUrl("href"));
-                }
-                return true;
+                System.out.println("\n**Visiting** Received web page at " + url);
             }
-            catch(IOException ioe)
-            {
-                // We were not successful in our HTTP request
+            if (!connection.response().contentType().contains("text/html")) {
+                System.out.println("**Failure** Retrieved something other than HTML");
                 return false;
             }
-        }
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            System.out.println("Found (" + linksOnPage.size() + ") links");
+            for (Element link : linksOnPage) {
+                String href = link.absUrl("href");
+                if(href.startsWith("https://gateway.ipfs.io/ipns/") && href.endsWith(".html")){
+                    System.out.println(href);
+                    crawl(href);
+                }
 
-
-        /**
-         * Performs a search on the body of on the HTML document that is retrieved. This method should
-         * only be called after a successful crawl.
-         *
-         * @param searchWord
-         *            - The word or string to look for
-         * @return whether or not the word was found
-         */
-        public boolean searchForWord(String searchWord)
-        {
-            // Defensive coding. This method should only be used after a successful crawl.
-            if(this.htmlDocument == null)
-            {
-                System.out.println("ERROR! Call crawl() before performing analysis on the document");
-                return false;
             }
-            System.out.println("Searching for the word " + searchWord + "...");
-            String bodyText = this.htmlDocument.body().text();
-            return bodyText.toLowerCase().contains(searchWord.toLowerCase());
+            return true;
+        } catch (IOException ioe) {
+            // We were not successful in our HTTP request
+            return false;
         }
+    }
 
 
-        public List<String> getLinks()
-        {
-            return this.links;
+    public boolean searchForWord(String searchWord) {
+        // Defensive coding. This method should only be used after a successful crawl.
+        if (this.htmlDocument == null) {
+            System.out.println("ERROR! Call crawl() before performing analysis on the document");
+            return false;
         }
+        System.out.println("Searching for the word " + searchWord + "...");
+        String bodyText = this.htmlDocument.body().text();
+        return bodyText.toLowerCase().contains(searchWord.toLowerCase());
+    }
+
+
 }
