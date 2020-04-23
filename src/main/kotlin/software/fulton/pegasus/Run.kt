2b606c9bb01a09c2269261d352338cb1b68d5577
fulton <gprlabs@outlook.com>
2020-04-22 19:49:48 -0700

    crawler first test is good now I have to hook up some domains

--- src/main/kotlin/software/fulton/pegasus/Run.kt
+++ src/main/kotlin/software/fulton/pegasus/Run.kt
@@ -1,4 +1,31 @@
 package software.fulton.pegasus
 
+import edu.uci.ics.crawler4j.crawler.CrawlConfig
+import edu.uci.ics.crawler4j.crawler.CrawlController
+import edu.uci.ics.crawler4j.crawler.CrawlController.WebCrawlerFactory
+import edu.uci.ics.crawler4j.fetcher.PageFetcher
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer
+
+
 object Run {
+    @JvmStatic
+    fun main(args: Array<String>){
+        println("go")
+        val numberOfCrawlers = 7
+
+        val config = CrawlConfig()
+        // Instantiate the controller for this crawl.
+
+        // Instantiate the controller for this crawl.
+        val pageFetcher = PageFetcher(config)
+        config.crawlStorageFolder = "/home/fulton/crawl"
+        val robotstxtConfig = RobotstxtConfig()
+        val robotstxtServer = RobotstxtServer(robotstxtConfig, pageFetcher)
+        val controller = CrawlController(config, pageFetcher, robotstxtServer)
+        controller.addSeed("fulton.eth")
+        val factory: WebCrawlerFactory<Spider> = WebCrawlerFactory { Spider() }
+        controller.start(factory, numberOfCrawlers);
+
+    }
 }
