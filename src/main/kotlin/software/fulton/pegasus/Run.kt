39868094eef5c3ecdc9ce9d7913baa80a75c1bae
fulton <gprlabs@outlook.com>
2020-05-09 15:25:08 -0700

    stuff

--- src/main/kotlin/software/fulton/pegasus/Run.kt
+++ src/main/kotlin/software/fulton/pegasus/Run.kt
@@ -17,7 +17,7 @@ object Run {
 
         // Instantiate the controller for this crawl.
         val spider = Spider()
-        spider.limit = 50000
+        spider.limit = 20000
         spider.crawl("https://gateway.ipfs.io/ipns/awesome.ipfs.io/")
         while (!spider.executorService.isShutdown);
         spider.executorService.awaitTermination(60, TimeUnit.SECONDS)
